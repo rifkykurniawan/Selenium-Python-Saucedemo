1385b107951d3bea1ed71182318aad7de40ce89f
Rifky Kurniawan <toettoet.tt@gmail.com>
2023-06-30 19:03:03 +0700

    Update saucedemo_testing.py

--- saucedemo_testing.py
+++ saucedemo_testing.py
@@ -23,44 +23,44 @@ class SauceDemoTest(unittest.TestCase):
         username_input.send_keys('standard_user')  # Enter the username
         password_input.send_keys('secret_sauce')  # Enter the password
         login_button.click()  # Click on the login button
-        sleep(2)
+
         
         # Add items to the cart
         product_elements = self.driver.find_elements(By.CSS_SELECTOR,  '.inventory_item')  # Find all product elements
         add_to_cart_buttons = self.driver.find_elements(By.CSS_SELECTOR,  '.btn_inventory')  # Find all "Add to Cart" buttons
-        sleep(2)
+
 
         for button in add_to_cart_buttons:
             button.click()  # Click on each "Add to Cart" button
-        sleep(2)
+  
         
         # Verify cart items count
         cart_count = self.driver.find_element(By.ID, 'shopping_cart_container')  # Find the cart items count
         self.assertEqual(int(cart_count.text), len(add_to_cart_buttons))  # Assert that the cart items count is correct
-        sleep(2)
+
         
         # Go to the cart
         cart_link = self.driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link')  # Find the cart link
         cart_link.click()  # Click on the cart link
-        sleep(2)
+   
 
         # Checkout
         checkout_button = self.driver.find_element(By.CSS_SELECTOR, '.btn_action.checkout_button')  # Find the checkout button
         checkout_button.click()  # Click on the checkout button
-        sleep(2)
+  
 
         # Provide checkout information
         first_name_input = self.driver.find_element(By.ID,'first-name')  # Find the first name input field
         last_name_input = self.driver.find_element(By.ID,'last-name')  # Find the last name input field
         postal_code_input = self.driver.find_element(By.ID,'postal-code')  # Find the postal code input field
-        sleep(2)
+   
 
         first_name_input.send_keys('Rifky')  # Enter the first name
-        sleep(1)
+      
         last_name_input.send_keys('Kurniawan')  # Enter the last name
-        sleep(1)
+     
         postal_code_input.send_keys('123456')  # Enter the postal code
-        sleep(1)
+       
 
         continue_button = self.driver.find_element(By.CSS_SELECTOR, '.btn_primary.cart_button')  # Find the continue button
         continue_button.click()  # Click on the continue button
